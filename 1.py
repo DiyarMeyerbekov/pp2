fcc803f60039728f1c6313cde410724ba1cb3e21
DiyarMeyerbekov <161132643+DiyarMeyerbekov@users.noreply.github.com>
2025-02-21 21:47:14 +0500

    Delete 1.py

--- 1.py
+++
@@ -1,6 +0,0 @@
-name=str(input("What is ur name "))
-age=int(input("Age "))
-if age>=18:
-    print(f"Name:{name},Age:{age}, Status:You are an adult")
-else:
-    print(f"Name:{name},Age:{age}, Status:You are a minor")
