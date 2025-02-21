f9f1687c19f5e33bdc08ec43dfbbea7e1d5e71a7
DiyarMeyerbekov <161132643+DiyarMeyerbekov@users.noreply.github.com>
2025-02-21 21:46:33 +0500

    Delete gohome.py

--- gohome.py
+++
@@ -1,9 +0,0 @@
-class me:
-    def __init__(self):
-        self.name=str(input("What is ur name? "))
-        self.surname=str(input("What is ur surname? "))
-        self.age=int(input("Ur age? "))
-    def out(self):
-        print(f"{self.name} {self.surname} born in {2025-self.age}")
-x=me()
-x.out()
